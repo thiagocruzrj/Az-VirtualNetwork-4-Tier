4692f8e66eda2fca621b991b59e05851a7a59d15
Thiago Cruz <thagocruz@gmail.com>
2022-06-09 19:47:11 -0300

    add terraform tfvars

--- input-variables.tf
+++ input-variables.tf
@@ -6,15 +6,18 @@ variable "business_division" {
 
 variable "env" {
   description = "Env variable used as a prefix"
+  type = string
   default = "dev"
 }
 
 variable "resource_group_name" {
   description = "Resource group name"
+  type = string
   default = "rg-default"
 }
 
 variable "resource_group_location" {
   description = "Region in which Resources to be created"
+  type = string
   default = "eastus2"
 }
