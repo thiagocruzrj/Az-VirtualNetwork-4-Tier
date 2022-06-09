433932838c0de9fb2c7c61a726d99ffb88445268
Thiago Cruz <thagocruz@gmail.com>
2022-06-09 19:24:13 -0300

    Add security rule

--- web-subnet-nsg.tf
+++ web-subnet-nsg.tf
@@ -12,6 +12,30 @@ resource "azurerm_network_security_group" "web_subnet_nsg" {
 }
 
 resource "azurerm_subnet_network_security_group_association" "web_subnet_nsg_association" {
+  depends_on = [ azurerm_network_security_rule.web_nsg_rule_inbound ]
   subnet_id = azurerm_subnet.websubnet.id
   network_security_group_id = azurerm_network_security_group.web_subnet_nsg.id
+}
+
+locals {
+  web_inbound_ports_map = {
+      "100" : "80",
+      "110" : "443",
+      "120" : "22"
+  }
+}
+
+resource "azurerm_network_security_rule" "web_nsg_rule_inbound" {
+  for_each                    = web_inbound_ports_map
+  name                        = "Rule-Port-${each.value}"
+  priority                    = each.key
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = each.value
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.web_subnet_nsg.name
 }
