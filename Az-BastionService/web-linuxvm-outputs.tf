c93e3c039157f50551cfa16ec8e1d80a3773431f
Thiago Cruz <thagocruz@gmail.com>
2022-07-05 16:25:16 -0300

    add bastion and llinuxvm variables

---
+++ Az-BastionService/web-linuxvm-outputs.tf
@@ -0,0 +1,26 @@
+# Network Interface Outputs
+output "web_linuxvm_network_interface_id" {
+  description = "Web Linux VM Network Interface ID"
+  value = azurerm_network_interface.web_linuxvm_nic.id
+}
+
+output "web_linuxvm_network_interface_private_ip_addresses" {
+  description = "Web Linux VM Private IP Addresses"
+  value = [azurerm_network_interface.web_linuxvm_nic.private_ip_addresses]
+}
+
+# Linux VM Outputs
+output "web_linuxvm_private_ip_address" {
+  description = "Web Linux Virtual Machine Private IP"
+  value = azurerm_linux_virtual_machine.web_linuxvm.private_ip_address
+}
+
+output "web_linuxvm_virtual_machine_id_128bit" {
+  description = "Web Linux Virtual Machine ID - 128-bit identifier"
+  value = azurerm_linux_virtual_machine.web_linuxvm.virtual_machine_id
+}
+
+output "web_linuxvm_virtual_machine_id" {
+  description = "Web Linux Virtual Machine ID "
+  value = azurerm_linux_virtual_machine.web_linuxvm.id
+}
