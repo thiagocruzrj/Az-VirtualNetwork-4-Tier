bf765ab56fbe76bcb3f13e957e4ffedb13ce8313
Thiago Cruz <thagocruz@gmail.com>
2022-07-05 14:19:35 -0300

    Add vm output

--- Az-VirtualMachine/output.tf
+++ Az-VirtualMachine/output.tf
@@ -1,29 +1,34 @@
-output "virtual_network_name" {
-  description = "Virtual Network Name"
-  value       = azurerm_virtual_network.vnet.name
+output "web_linuxvm_publicip" {
+  description = "Web Linux VM Public Address"
+  value       = azurerm_public_ip.web_linuxvm_publicip.ip_address
 }
 
-output "virtual_network_id" {
-  description = "Virtual Network ID"
-  value       = azurerm_virtual_network.vnet.id
+output "web_linuxvm_network_interface_id" {
+  description = "Web Linux VM Network Interface ID"
+  value       = azurerm_network_interface.web_linuxvm_nic.id
 }
 
-output "web_subnet_name" {
-  description = "WebTier Subnet Name"
-  value       = azurerm_subnet.websubnet.name
+output "web_linuxvm_network_interface_private_ip_addresses" {
+  description = "Web Linux VM Network Interface ID"
+  value       = [azurerm_network_interface.web_linuxvm_nic.private_ip_addresses]
 }
 
-output "web_subnet_id" {
-  description = "WebTier Subnet ID"
-  value       = azurerm_subnet.websubnet.id
+output "web_linuxvm_public_ip_address" {
+  description = "Web Linux Virtual Machine Public IP"
+  value = azurerm_linux_virtual_machine.web_linuxvm.public_ip_address
 }
 
-output "web_subnet_nsg_name" {
-  description = "WebTier Subnet NSG Name"
-  value       = azurerm_network_security_group.web_subnet_nsg.name
+output "web_linuxvm_private_ip_address" {
+  description = "Web Linux Virtual Machine Private IP"
+  value = azurerm_linux_virtual_machine.web_linuxvm.private_ip_address
 }
 
-output "web_subnet_nsg_id" {
-  description = "WebTier Subnet NSG ID"
-  value       = azurerm_network_security_group.web_subnet_nsg.id
+output "web_linuxvm_virtual_machine_id_128bit" {
+  description = "Web Linux Virtual Machine ID - 128-bit identifier"
+  value = azurerm_linux_virtual_machine.web_linuxvm.virtual_machine_id
+}
+
+output "web_linuxvm_virtual_machine_id" {
+  description = "Web Linux Virtual Machine ID "
+  value = azurerm_linux_virtual_machine.web_linuxvm.id
 }
