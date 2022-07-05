6967e6d158640558794fc6d3a8cf5df8aaa71f94
Thiago Cruz <thagocruz@gmail.com>
2022-07-05 14:03:55 -0300

    Add custom data commands and script

--- Az-VirtualMachine/web-linuxvm-resource.tf
+++ Az-VirtualMachine/web-linuxvm-resource.tf
@@ -1,3 +1,22 @@
+locals {
+webvm_custom_data = <<CUSTOM_DATA
+#!/bin/sh
+#sudo yum update -y
+sudo yum install -y httpd
+sudo systemctl enable httpd
+sudo systemctl start httpd  
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+sudo chmod -R 777 /var/www/html 
+sudo echo "Welcome to stacksimplify - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/index.html
+sudo mkdir /var/www/html/app1
+sudo echo "Welcome to stacksimplify - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/app1/hostname.html
+sudo echo "Welcome to stacksimplify - WebVM App1 - App Status Page" > /var/www/html/app1/status.html
+sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Welcome to Stack Simplify - WebVM APP-1 </h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
+sudo curl -H "Metadata:true" --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2020-09-01" -o /var/www/html/app1/metadata.html
+CUSTOM_DATA
+}
+
 resource "azurerm_linux_virtual_machine" "web_linuxvm" {
   name = "${local.resource_name_prefix}-web-linuxvm"
   computer_name = "web-linux-vm"
@@ -20,4 +39,5 @@ resource "azurerm_linux_virtual_machine" "web_linuxvm" {
     sku = "83-gen2"
     version = "latest"
   }
+  custom_data = base64encode(local.webvm_custom_data)
 }
